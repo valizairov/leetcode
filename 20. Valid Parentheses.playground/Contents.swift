cd4c7555d0fe3ab1232f423160fb3274f1cf2d81
Vali Zairov <zairov@gmail.com>
2023-08-04 23:48:06 -0700

    20. Valid Parentheses - Optimized

--- 20. Valid Parentheses.playground/Contents.swift
+++ 20. Valid Parentheses.playground/Contents.swift
@@ -1,5 +1,5 @@
 /*
- 20. Valid Parentheses Easy
+ 20. Valid Parentheses - Easy
 
 Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
 
@@ -8,25 +8,45 @@ An input string is valid if:
 Open brackets must be closed by the same type of brackets.
 Open brackets must be closed in the correct order.
 Every close bracket has a corresponding open bracket of the same type.
- 
 
 Example 1:
 Input: s = "()"
 Output: true
-                                                                                                
+                                                                 
 Example 2:
 Input: s = "()[]{}"
 Output: true
-                                                                                                
+                                                            
 Example 3:
 Input: s = "(]"
 Output: false
 */
 
 class Solution {
+    class Stack {
+        private var array = [String]()
+        func push(_ s: String) {
+            array.append(s)
+        }
+        func pop() -> String? {
+            return array.popLast()
+        }
+        func peek() -> String? {
+            return array.last
+        }
+    }
+    
     func isValid(_ s: String) -> Bool {
-        //use stacks to solve this
-        return true
+        let stack = Stack()
+        let map = [")": "(", "]": "[", "}": "{"]
+        for c in s {
+            if let ch = map[String(c)], ch == stack.peek() {
+                stack.pop()
+            } else {
+                stack.push(String(c))
+            }
+        }
+        return stack.peek() == nil
     }
 }
 
@@ -34,4 +54,4 @@ class Solution {
 Solution().isValid("()") //true
 Solution().isValid("()[]{}") //true
 Solution().isValid("(]") //false
-
+Solution().isValid("([{}])") //true
