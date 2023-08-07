047e651ee81d3375998a406b0ab980f3d98b5f62
Vali Zairov <zairov@gmail.com>
2023-08-06 22:15:37 -0700

    22. Generate Parentheses - started

--- 22. Generate Paranthesis.playground/Contents.swift
+++ 22. Generate Paranthesis.playground/Contents.swift
@@ -1,3 +1,21 @@
-import UIKit
+/*
+22. Generate Parentheses - Medium
 
-var greeting = "Hello, playground"
+Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+Example 1:
+Input: n = 3
+Output: ["((()))","(()())","(())()","()(())","()()()"]
+ 
+Example 2:
+Input: n = 1
+Output: ["()"]
+ 
+*/
+
+class Solution {
+    func generateParenthesis(_ n: Int) -> [String] {
+        
+        return []
+    }
+}
