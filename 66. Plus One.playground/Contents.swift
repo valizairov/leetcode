3ae2c707cd6d23e4cd156dc15ec62163239103aa
Vali Zairov <zairov@gmail.com>
2023-08-23 00:27:38 -0700

    66. Plus One - Easy - Fixed type in one of the cases

--- 66. Plus One.playground/Contents.swift
+++ 66. Plus One.playground/Contents.swift
@@ -55,5 +55,5 @@ class Solution {
 Solution().plusOne([1,2,3]) //[1,2,4]
 Solution().plusOne([4,3,2,1]) //[4,3,2,2]
 Solution().plusOne([9]) //[1,0]
-Solution().plusOne([9,9]) //[1,0]
+Solution().plusOne([9,9]) //[1,0,0]
 
