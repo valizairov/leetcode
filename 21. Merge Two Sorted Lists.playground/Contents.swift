3773647a04d04b526fe35678069e3dab3de8a08f
Vali Zairov <zairov@gmail.com>
2023-08-26 22:59:45 -0700

    69. Sqrt(x) - Easy - Started

--- 21. Merge Two Sorted Lists.playground/Contents.swift
+++ 21. Merge Two Sorted Lists.playground/Contents.swift
@@ -25,7 +25,7 @@ public class ListNode {
     public init() { self.val = 0; self.next = nil; }
     public init(_ val: Int) { self.val = val; self.next = nil; }
     public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next;}
-}   
+}
 
 class Solution {
     func mergeTwoLists(_ list1: ListNode?, _ list2: ListNode?) -> ListNode? {
