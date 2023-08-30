91fb06072e0f04f347bebfcdc482b6d4cb80340e
Vali Zairov <zairov@gmail.com>
2023-08-29 23:33:52 -0700

    189. Rotate Array - Medium - Completed

--- 189. Rotate Array.playground/Contents.swift
+++ 189. Rotate Array.playground/Contents.swift
@@ -21,6 +21,28 @@
 
 class Solution {
     func rotate(_ nums: inout [Int], _ k: Int) {
-        
+        guard nums.count > 1 else { return }
+        let k = k % nums.count
+        var array1 = [Int]()
+        var array2 = [Int]()
+        for i in (nums.count - k)..<nums.count {
+            array1.append(nums[i])
+        }
+        for i in 0..<nums.count-k {
+            array2.append(nums[i])
+        }
+        nums = array1 + array2
     }
 }
+
+var array1 = [1,2,3,4,5,6,7]
+Solution().rotate(&array1, 3) // [5,6,7,1,2,3,4]
+//print(array1)
+
+var array2 = [-1,-100,3,99]
+Solution().rotate(&array2, 2)  // [3,99,-1,-100]
+//print(array2)
+
+var array3 = [1,2]
+Solution().rotate(&array3, 3)  // [2,1]
+print(array3)
