e7ff1c337766188ab1c4ec63a39197ba25eaf113
Vali Zairov <zairov@gmail.com>
2023-09-02 09:21:18 -0700

    169. Majority Element - Easy - Solved

--- 169. Majority Element.playground/Contents.swift
+++ 169. Majority Element.playground/Contents.swift
@@ -16,9 +16,21 @@
 
 class Solution {
     func majorityElement(_ nums: [Int]) -> Int {
+        guard nums.count > 0 else { return 0 }
+        guard nums.count > 1 else { return nums[0] }
+        var dict = [Int: Int]()
+        for e in nums {
+            if dict[e] != nil {
+                dict[e]! += 1
+                if dict[e]! > nums.count/2 { return e }
+            } else {
+                dict[e] = 1
+            }
+        }
         return 0
     }
 }
 
 Solution().majorityElement([3,2,3]) //3
 Solution().majorityElement([2,2,1,1,1,2,2]) //2
+Solution().majorityElement([1]) //1
