af689db789afa580c3ec95ef96b36ef9e7681265
Vali Zairov <zairov@gmail.com>
2023-09-03 23:54:46 -0700

    168. Excel Sheet Column Title - Easy - made some progress

--- 168. Excel Sheet Column Title.playground/Contents.swift
+++ 168. Excel Sheet Column Title.playground/Contents.swift
@@ -27,18 +27,37 @@
  Output: "ZY"
  */
 
+
 class Solution {
     func convertToTitle(_ columnNumber: Int) -> String {
-        let letters = [ "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-        guard columnNumber > 26 else { return letters[columnNumber-1] }
-        let a = letters[columnNumber/26-1]
-        let b = letters[columnNumber%26-1]
-        return a + b
+        let letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+        let base = 26
+        var columnNumber = columnNumber
+        var res = ""
+
+        guard columnNumber > base else { return letters[columnNumber-1] }
+        
+        while columnNumber >= base {
+            print(columnNumber)
+            res += letters[columnNumber % base]
+            columnNumber = columnNumber/base
+        }
+        res = res + letters[columnNumber]
+        print("------------------")
+        return res
     }
 }
 
-Solution().convertToTitle(1) //A
-Solution().convertToTitle(28) //AB
+//146 -> 5.6
+//701 -> ZY // 26.96
+//702 -> ZZ // 27
+//703 -> AAA //27.03
+
+//Solution().convertToTitle(1) //A
+//Solution().convertToTitle(28) //AB
+//Solution().convertToTitle(146) //EP
 Solution().convertToTitle(701) //ZY
+//Solution().convertToTitle(702) //ZZ
+//Solution().convertToTitle(703) //AAA
 //Solution().convertToTitle(2147483647) //last case not working
 
