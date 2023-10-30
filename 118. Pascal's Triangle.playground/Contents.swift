f2a82091a8b25ebf7864ce9b342d0890c81f9d67
valizairov <38022503+valizairov@users.noreply.github.com>
2023-10-29 23:52:53 -0700

    Update Contents.swift

--- 118. Pascal's Triangle.playground/Contents.swift
+++ 118. Pascal's Triangle.playground/Contents.swift
@@ -1,6 +1,6 @@
 /*
  118. Pascal's Triangle - Easy - 1
-      
+       
  Given an integer numRows, return the first numRows of Pascal's triangle.
  In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
   
