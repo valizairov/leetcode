180b2666326804ae67cccdc063e505acff05b4ff
Vali Zairov <zairov@gmail.com>
2024-01-07 22:48:35 -0800

    1002. Find Common Characters - Easy - Started

---
+++ 1002. Find Common Characters.playground/Contents.swift
@@ -0,0 +1,23 @@
+/*
+ 1002. Find Common Characters - Easy
+ 
+ Given a string array words, return an array of all characters that show up in all strings within the words (including duplicates).
+ You may return the answer in any order.
+
+ Example 1:
+ Input: words = ["bella","label","roller"]
+ Output: ["e","l","l"]
+ 
+ Example 2:
+ Input: words = ["cool","lock","cook"]
+ Output: ["c","o"]
+
+ */
+class Solution {
+    func commonChars(_ words: [String]) -> [String] {
+        return []
+    }
+}
+
+
+let result = Solution().commonChars(["bella","label","roller"]) //["e","l","l"]
