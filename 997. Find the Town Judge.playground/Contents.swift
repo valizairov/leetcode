919c25fd4a2a97b5afce194ee9b40e13dddfca5c
Vali Zairov <zairov@gmail.com>
2024-01-08 23:54:16 -0800

    Passed all cases in description and was run fine in the leetcode, but failed on test cases. Looks like I didn't get the problem well.

--- 997. Find the Town Judge.playground/Contents.swift
+++ 997. Find the Town Judge.playground/Contents.swift
@@ -27,8 +27,21 @@
 
 class Solution {
     func findJudge(_ n: Int, _ trust: [[Int]]) -> Int {
-        return -1
+        guard n > 0 else { return -1 }
+        var judge: Int?
+        for pair in trust {
+            if judge == nil {
+                judge = pair[1]
+            } else {
+                if judge != pair[1] {
+                    return -1
+                }
+            }
+        }
+        return judge!
     }
 }
 
 let result = Solution().findJudge(3, [[1,3],[2,3],[3,1]])
+let result2 = Solution().findJudge(2, [[1,2]])
+let result3 = Solution().findJudge(3, [[1,3],[2,3]])
