6b38495881f2c0a71818a39001fde3954122d102
Vali Zairov <zairov@gmail.com>
2024-01-08 22:51:29 -0800

    added extension, added edge cases, started the loop

--- 1002. Find Common Characters.playground/Contents.swift
+++ 1002. Find Common Characters.playground/Contents.swift
@@ -13,8 +13,22 @@
  Output: ["c","o"]
 
  */
+
+extension String {
+    subscript(i: Int) -> String {
+        return String(self[index(startIndex, offsetBy: i)])
+    }
+}
+
 class Solution {
     func commonChars(_ words: [String]) -> [String] {
+        if words.count == 0 { return [] }
+        let word = words[0]
+        if word == "" { return [] }
+        if word.count == 1 { return [String(word[0])] }
+        for i in 1..<words.count {
+            print(words[i])
+        }
         // Possible way is to use the dictionary of characters in 1st word, and look at them at each next word and eliminate if not found.
         return []
     }
