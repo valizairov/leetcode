a5a7a5a3d11f25391de05b110346865600adda17
valizairov <38022503+valizairov@users.noreply.github.com>
2024-04-29 23:32:09 -0700

    Update Contents.swift

--- 852. Peak Index in a Mountain Array.playground/Contents.swift
+++ 852. Peak Index in a Mountain Array.playground/Contents.swift
@@ -8,7 +8,7 @@
  Given a mountain array arr, return the index i such that arr[0] < arr[1] < ... < arr[i - 1] < arr[i] > arr[i + 1] > ... > arr[arr.length - 1].
 
  You must solve it in O(log(arr.length)) time complexity.
- 
+  
  Example 1:
  Input: arr = [0,1,0]
  Output: 1
