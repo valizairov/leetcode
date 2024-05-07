a561533d9ced638d5f612a59f38188e4b5d1d939
valizairov <38022503+valizairov@users.noreply.github.com>
2024-05-06 23:54:14 -0700

    Update Contents.swift

--- 21. Merge Two Sorted Lists.playground/Contents.swift
+++ 21. Merge Two Sorted Lists.playground/Contents.swift
@@ -1,6 +1,6 @@
 /*
  21. Merge Two Sorted Lists - Easy
- 
+  
  You are given the heads of two sorted linked lists list1 and list2.
  Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
  Return the head of the merged linked list.
