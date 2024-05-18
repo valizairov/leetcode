26881bcdb8f9468c6d85e49ac7105d34609f757a
valizairov <38022503+valizairov@users.noreply.github.com>
2024-05-17 23:00:05 -0700

    Update Contents.swift

--- 735. Asteroid Collision.playground/Contents.swift
+++ 735. Asteroid Collision.playground/Contents.swift
@@ -1,6 +1,6 @@
 /*
  735. Asteroid Collision - Medium
-  
+   
  We are given an array asteroids of integers representing asteroids in a row.
 
  For each asteroid, the absolute value represents its size, and the sign represents its direction (positive meaning right, negative meaning left). Each asteroid moves at the same speed.
