8844bb45f4e40c69be0acf335525e52bea400bb1
valizairov <38022503+valizairov@users.noreply.github.com>
2024-07-14 22:10:34 -0700

    Update Contents.swift

--- 22. Generate Paranthesis.playground/Contents.swift
+++ 22. Generate Paranthesis.playground/Contents.swift
@@ -2,7 +2,7 @@
 22. Generate Parentheses - Medium
 
 Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
- 
+  
 Example 1:
 Input: n = 3
 Output: ["((()))","(()())","(())()","()(())","()()()"]
